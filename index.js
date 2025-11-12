6ed9f475c887e8f0f8eb180c28069a5f26bbdac1
Piyush2679 <155120175+Piyush2679@users.noreply.github.com>
2025-11-12 13:25:43 +0530

    Update response message for Jenkins CI/CD testing
    Signed-off-by: Piyush2679 <155120175+Piyush2679@users.noreply.github.com>

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!, This is testing Jenkins CI/CD Pipeline')
+  response.send('Hello World!, This is testing Jenkins CI/CD Pipeline automation deployement')
 })
 
 app.listen(app.get('port'), function() {
